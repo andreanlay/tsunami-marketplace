e29bbc8309ff4380777fcdbc96e0f9ddb6301324
andreanlay <andreanlay1@gmail.com>
2020-10-10 13:49:40 +0700

    Finish page 1 of checkout

---
+++ src/js/payment.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.payments div').filter('#form-creditcard-payment').fadeIn(400).siblings('div').hide();
+    $('#checkout-p2').hide();
+
+    $('#payment-selector').on('change', function() {
+        let id = $(this).find(':selected').attr('id');
+        $('.payments div').filter('#form-' + id).fadeIn(400).siblings('div').hide();
+    });
+
+    $('#btn-next').on('click', function() {
+        $('body div').filter('#checkout-p2').fadeIn(500).siblings('div').hide();
+    });
+});
