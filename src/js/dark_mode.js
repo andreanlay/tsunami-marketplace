1ec5021eb92cb15033642ea02c687275f719ce34
andreanlay <andreanlay1@gmail.com>
2020-10-16 11:37:16 +0700

    Remove dark mode localStorage modifier

--- src/js/dark_mode.js
+++ src/js/dark_mode.js
@@ -2,17 +2,5 @@ $(document).ready(function() {
     var theme = JSON.parse(localStorage.getItem('theme'));
     if(theme == 'dark') {
         $('body').attr('data-theme', 'dark');
-        if($('#darkmode-switch').length) {
-            $('#darkmode-switch').click();
-        }
     }
-    $('#darkmode-switch').on('change', function() {
-        if($(this).is(':checked')) {
-            localStorage.setItem('theme', JSON.stringify('dark'));
-            $('body').attr('data-theme', 'dark');
-        } else {
-            localStorage.removeItem('theme');
-            $('body').removeAttr('data-theme');
-        }
-    });
 })
