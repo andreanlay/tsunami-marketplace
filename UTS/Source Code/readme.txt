d4531b9acdc0ac5316cf5072f21b1f8e7ed8da98
andreanlay <andreanlay1@gmail.com>
2020-10-29 13:08:41 +0700

    Remove folders

--- UTS/Source Code/readme.txt
+++
@@ -1,2 +0,0 @@
-Web dapat diakses secara langsung di:
-https://andreanlay.github.io/tsunami-marketplace/
--- src/js/dark_mode.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    var theme = JSON.parse(localStorage.getItem('theme'));
-    if(theme == 'dark') {
-        $('body').attr('data-theme', 'dark');
-    }
-})
