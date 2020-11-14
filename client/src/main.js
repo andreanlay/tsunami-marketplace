0064a4bfce2c1fcc718d367b5b0a70c733c3e007
andreanlay <andreanlay1@gmail.com>
2020-11-14 23:44:10 +0700

    Add firebase config

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import titleMixin from './mixins/titleMixins'
 import store from './store/store'
+import firebase from 'firebase/app'
 
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,6 +13,19 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.mixin(titleMixin)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCux04fNQ85Stpk2MsUZXYYd92_rWZDF_o",
+  authDomain: "tsunami-bcb28.firebaseapp.com",
+  databaseURL: "https://tsunami-bcb28.firebaseio.com",
+  projectId: "tsunami-bcb28",
+  storageBucket: "tsunami-bcb28.appspot.com",
+  messagingSenderId: "1088125828395",
+  appId: "1:1088125828395:web:0d4396efbe5d7b9d49996e",
+  measurementId: "G-Z2N33M9DMJ"
+}
+
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
