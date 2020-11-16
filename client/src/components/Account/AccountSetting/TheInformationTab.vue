d5560f8d9038a29ec94ec71aa4214d39a32b53c3
andreanlay <andreanlay1@gmail.com>
2020-11-16 20:31:16 +0700

    Connect account information tab to backend

--- client/src/components/Account/AccountSetting/TheInformationTab.vue
+++ client/src/components/Account/AccountSetting/TheInformationTab.vue
@@ -1,55 +1,139 @@
 <template>
 <div class="d-flex flex-column align-items-start">
-    <h1 class="display-7 info-header">Personal Information <b-icon icon="pencil" font-scale="0.75"></b-icon></h1>
+    <h1 class="display-7 info-header">
+        Personal Information
+        <a href="#"><b-badge @click="editPersonalInfo()" variant="light">Edit</b-badge></a>
+    </h1>
     <div class="data-row">
         <p class="row-label">Full Name</p>
-        <p class="row-data">{{username}}</p>
+        <input v-model="accountData.displayName" :class="{'row-input-active' : editingPersonalDetails}" type="text" class="row-input" :disabled="!editingPersonalDetails">
     </div>
     <div class="data-row">
         <p class="row-label">Phone Number</p>
-        <p class="row-data">{{phone}}</p>
+        <input v-model="accountData.phone_number" :class="{'row-input-active' : editingPersonalDetails}" type="text" class="row-input" :disabled="!editingPersonalDetails">
     </div>
     <div class="data-row">
         <p class="row-label">Email Address</p>
-        <p class="row-data">{{email}}</p>
+        <input v-model="accountData.email" :class="{'row-input-active' : editingPersonalDetails}" type="text" class="row-input" :disabled="!editingPersonalDetails">
     </div>
     <div class="data-row">
         <p class="row-label">Birthday</p>
-        <p class="row-data">{{birthday}}</p>
+        <p class="row-data">{{accountData.birthday}}</p>
     </div>
     <div class="data-row">
         <p class="row-label">Gender</p>
-        <p class="row-data">{{gender}}</p>
+        <p class="row-data">{{accountData.gender}}</p>
     </div>
+    <b-button
+        v-show="editingPersonalDetails" 
+        :class="{'update-active' : updatingPersonalDetails}" 
+        variant="primary" 
+        class="align-self-end mr-4 mb-4"
+        @click="savePersonalDetails"
+    >
+        <b-icon icon="check"></b-icon> {{updatingPersonalDetails ? "Saving..." : "Save"}}
+    </b-button>
 
-        <h1 class="display-7 info-header">Seller Information <b-icon icon="pencil" font-scale="0.75"></b-icon></h1>
+    <h1 class="display-7 info-header">
+        Seller Information
+        <a href="#"><b-badge @click="editSellerInfo()" variant="light">Edit</b-badge></a>
+    </h1>
     <div class="data-row">
         <p class="row-label">Shop Name</p>
-        <p class="row-data">{{shopName}}</p>
+        <input v-model="accountData.seller.name" :class="{'row-input-active' : editingSellerDetails}" type="text" class="row-input" :disabled="!editingSellerDetails">
+    </div>
+    <div class="data-row">
+        <p class="row-label">Shop Location</p>
+        <input v-model="accountData.seller.location" :class="{'row-input-active' : editingSellerDetails}" type="text" class="row-input" :disabled="!editingSellerDetails">
     </div>
     <div class="data-row">
         <p class="row-label">Products Sold</p>
-        <p class="row-data">{{productsSold}}</p>
+        <input v-model="productsSold" :class="{'row-input-active' : editingSellerDetails}" type="text" class="row-input" :disabled="!editingSellerDetails">
     </div>
-    <div class="data-row">
-        <p class="row-label">Shop Status</p>
-        <p class="row-data" style="color: green">{{shopStatus}}</p>
-    </div>
+    <b-button
+        v-show="editingSellerDetails" 
+        :class="{'update-active' : updatingSellerDetails}" 
+        variant="primary" 
+        class="align-self-end mr-4 mb-4"
+        @click="saveSellerDetails"
+    >
+        <b-icon icon="check"></b-icon> {{updatingSellerDetails ? "Saving..." : "Save"}}
+    </b-button>
 </div>
 </template>
 
 <script>
+import firebase from 'firebase/app'
+import axios from 'axios'
+import { mapGetters } from 'vuex'
+
 export default {
     data() {
         return {
-            username: 'Irfan Bachdim',
-            gender: 'Male',
-            phone: '+62 856 5123 1237',
-            email: 'irfanbch@gmail.com',
-            birthday: 'October 20, 1998',
-            shopName: 'Jantono Shop',
             productsSold: 312,
-            shopStatus: 'Active'
+            editingPersonalDetails: false,
+            editingSellerDetails: false,
+            updatingPersonalDetails: false,
+            updatingSellerDetails: false,
+        }
+    },
+    computed: {
+        ...mapGetters(['accountData'])
+    },
+    methods: {
+        updateStore() {
+            const user = firebase.auth().currentUser
+            this.$store.commit('accountData', {
+                uid: user.uid,
+                displayName: user.displayName,
+                email: user.email,
+                phone_number: this.accountData.phone_number,
+                birthday: this.accountData.birthday,
+                gender: this.accountData.gender,
+                seller: {
+                    name: this.accountData.seller.name,
+                    location: this.accountData.seller.location,
+                }
+            })
+        },
+        editPersonalInfo() {
+            this.editingPersonalDetails = !this.editingPersonalDetails
+        },
+        editSellerInfo() {
+            this.editingSellerDetails = !this.editingSellerDetails
+        },
+        async savePersonalDetails() {
+            this.updatingPersonalDetails = true
+
+            const user = firebase.auth().currentUser
+            if(user.email != this.accountData.email) {
+                user.updateEmail(this.accountData.email)
+                user.sendEmailVerification()
+            }
+            if(user.displayName != this.accountData.displayName) {
+                user.updateProfile({
+                    displayName: this.accountData.displayName
+                })
+            }
+            await axios.put(`/api/auth/${this.accountData.uid}`, {
+                phone_number: this.accountData.phone_number
+            })
+            this.updateStore()
+            this.editingPersonalDetails = !this.editingPersonalDetails
+            this.updatingPersonalDetails = false
+        },
+        async saveSellerDetails() {
+            this.updatingSellerDetails = true
+
+            await axios.put(`/api/auth/${this.accountData.uid}`, {
+                seller: {
+                    name: this.accountData.seller.name,
+                    location: this.accountData.seller.location
+                }
+            })
+            this.updateStore()
+            this.editingSellerDetails = !this.editingSellerDetails
+            this.updatingSellerDetails = false
         }
     }
 }
@@ -89,5 +173,25 @@ export default {
 .row-data {
     width: 40%;
     text-align: left;
+}
+
+.row-input {
+    /* width: 40%; */
+    height: 20px;
+    border: none;
+    background: none;
+    padding-bottom: 5px;
+}
+
+.row-input-active {
+    border-bottom: 1px solid lightslategray;
+}
+
+.row-input:focus {
+    outline: none;
+}
+
+.update-active {
+    opacity: 0.6;
 }
 </style>
