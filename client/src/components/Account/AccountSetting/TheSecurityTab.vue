7288454674386cdf416e41de11b8d480874533b3
andreanlay <andreanlay1@gmail.com>
2020-11-16 20:30:52 +0700

    Connect change password & PIN to backend

--- client/src/components/Account/AccountSetting/TheSecurityTab.vue
+++ client/src/components/Account/AccountSetting/TheSecurityTab.vue
@@ -3,69 +3,140 @@
     <h1 class="display-7 info-header">Password</h1>
     <div class="data-row">
         <p class="row-label">Current Password</p>
-        <input type="password" class="row-input">
+        <input v-model="password" type="password" class="row-input">
     </div>
     <div class="data-row">
         <p class="row-label">New Password</p>
-        <input v-model="newPassword" @change="checkPassword" type="password" class="row-input">
+        <input v-model="newPassword" type="password" class="row-input">
     </div>
     <div class="data-row">
         <p class="row-label">Confirm New Password</p>
-        <input v-model="confirmPassword" @change="checkPassword" type="password" class="row-input">
+        <input v-model="confirmPassword" type="password" class="row-input">
     </div>
-    <p style="color: red" v-show="passwordNotSame">Password & Confirm password is not the same</p>
-    <b-button variant="primary" class="align-self-end">Update</b-button>
+    <p style="color: red" class="align-self-end" v-show="passwordInfo">{{passwordInfo}}</p>
+    <p style="color: green" class="align-self-end" v-show="passwordChanged">Password changed successfuly!</p>
+    <b-button 
+        variant="primary" 
+        class="align-self-end" 
+        :class="{'update-loading': changePasswordLoading}"
+        @click="updatePassword"
+    >
+        {{changePasswordLoading ? 'Updating...' : 'Update'}}
+    </b-button>
 
     <h1 class="display-7 info-header">Tsunami PIN</h1>
     <div class="data-row">
         <p class="row-label">Current PIN</p>
-        <input type="password" class="row-input">
+        <input v-model="pin" type="password" class="row-input">
     </div>
     <div class="data-row">
         <p class="row-label">New PIN</p>
-        <input v-model="newPIN" @change="checkPIN" type="password" class="row-input">
+        <input v-model="newPIN" type="password" class="row-input">
     </div>
     <div class="data-row">
         <p class="row-label">Confirm New PIN</p>
-        <input v-model="confirmPIN" @change="checkPIN" type="password" class="row-input">
+        <input v-model="confirmPIN" type="password" class="row-input">
     </div>
-    <p style="color: red" v-show="pinNotSame">PIN & Confirm PIN is not the same</p>
-    <b-button variant="primary" class="align-self-end">Update</b-button>
+    <p style="color: red" class="align-self-end" v-show="pinInfo">{{pinInfo}}</p>
+    <p style="color: green" class="align-self-end" v-show="pinChanged">PIN changed successfuly!</p>
+    <b-button 
+        variant="primary" 
+        class="align-self-end" 
+        :class="{'update-loading': changePINLoading}"
+        @click="updatePIN"
+    >
+        {{changePINLoading ? 'Updating...' : 'Update'}}
+    </b-button>
 </div>
 </template>
 
 <script>
+import firebase from 'firebase/app'
+import axios from 'axios'
+import { mapGetters } from 'vuex'
+
 export default {
     data() {
         return {
+            password: '',
             newPassword: '',
             confirmPassword: '',
-            passwordNotSame: false,
+            passwordInfo: '',
+            passwordChanged: false,
+            changePasswordLoading: false,
+            pin: '',
             newPIN: '',
             confirmPIN: '',
-            pinNotSame: false,
+            pinInfo: '',
+            pinChanged: false,
+            changePINLoading: false,
         }
+    },
+    computed: {
+        ...mapGetters(['accountData'])
     },
     methods: {
         checkPassword() {
-            if(this.newPassword != this.confirmPassword) {
-                this.passwordNotSame = true
-            }else{
-                this.passwordNotSame = false
-            }
+            return this.newPassword == this.confirmPassword
         },
         checkPIN() {
-            if(this.newPIN != this.confirmPIN) {
-                this.pinNotSame = true
-            }else{
-                this.pinNotSame = false
+            return this.newPIN == this.confirmPIN
+        },
+        async updatePassword() {
+            this.changePasswordLoading = true
+            this.passwordChanged = false
+
+            if(this.checkPassword()) {
+                const user = firebase.auth().currentUser
+                const credential = firebase.auth.EmailAuthProvider.credential(this.accountData.email, this.password)
+
+                const self = this
+                await user.reauthenticateWithCredential(credential)
+                .then(function() {
+                    user.updatePassword(self.newPassword).then(function() {
+                        self.passwordInfo = ''
+                        self.passwordChanged = true
+                    })
+                    .catch(err => {
+                        self.passwordInfo = err.message
+                    })
+                })
+                .catch(err => {
+                    this.passwordInfo = err.message
+                })
+            } else {
+                this.passwordInfo = 'New password & Confirm password is not the same'
             }
+            this.changePasswordLoading = false
+        },
+        async updatePIN() {
+            this.changePINLoading = true
+            this.pinChanged = false
+
+            if(this.checkPIN()) {
+                console.log(this.accountData.PIN)
+                if(this.accountData.PIN == null || this.accountData.PIN == this.pin) {
+                    await axios.put(`/api/auth/${this.accountData.uid}`, {
+                        PIN: this.newPIN
+                    })
+                    this.$store.commit('changePIN', {
+                        PIN: this.newPIN
+                    })
+                    this.pinChanged = true
+                    this.pinInfo = ''
+                } else {
+                    this.pinInfo = 'Wrong PIN, please check and try again'
+                }
+            } else {
+                this.pinInfo = 'New PIN & Confirm PIN is not the same'
+            }
+            this.changePINLoading = false
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
 .row-input {
     width: 40%;
     height: 20px;
@@ -76,5 +147,9 @@ export default {
 
 .row-input:focus {
     outline: none;
+}
+
+.update-loading {
+    opacity: 0.6
 }
 </style>
