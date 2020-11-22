de7ba49ec11c2ef28c96269b2ad4d14c1fe6bbbb
andreanlay <andreanlay1@gmail.com>
2020-11-22 22:26:41 +0700

    Finish product schema

---
+++ models/Product.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose')
+
+const ProductSchema = new Schema({
+    seller_uid: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    subcategory: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: Array,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    detailed_description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    specifications: {
+        type: Array,
+        required: true
+    }
+})
+
+const Product = model('product', ProductSchema)
+
+module.exports = Product
