e22015aed22684b6e1277a754ef618c009074934
andreanlay <andreanlay1@gmail.com>
2020-11-22 22:24:03 +0700

    Add initial product routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fileUpload = require('express-fileupload')
 const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
@@ -7,10 +8,12 @@ const app = express()
 
 const authRoute = require('./routes/api/Auth.js')
 const addressRoute = require('./routes/api/Address.js')
+const productRoute = require('./routes/api/Product.js')
 
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(fileUpload())
 
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
@@ -22,5 +25,6 @@ mongoose.connect(mongoUri, {
 
 app.use('/api/auth', authRoute)
 app.use('/api/address', addressRoute)
+app.use('/api/product', productRoute)
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
