483e41ad15a66209156e5901cfe196c96bd9707b
andreanlay <andreanlay1@gmail.com>
2020-11-29 19:23:17 +0700

    Add payment and shipping address store

--- client/src/store/modules/payment.js
+++ client/src/store/modules/payment.js
@@ -1,51 +1,31 @@
 const state = {
     method: [
-        {
-            text: 'Credit Card',
-            value: 'creditcard'
-        },
-        {
-            text: 'Bank Transfer',
-            value: 'banktransfer'
-        },
-        {
-            text: 'Cash on Delivery',
-            value: 'cod'
-        },
-        {
-            text: 'OVO',
-            value: 'ovo'
-        },
-        {
-            text: 'GoPay',
-            value: 'gopay'
-        }
+        { text: 'Credit Card', value: 'Credit Card' },
+        { text: 'Bank Transfer', value: 'Bank Transfer' },
+        { text: 'Cash on Delivery', value: 'Cash on Delivery (COD)' },
+        { text: 'OVO', value: 'OVO' },
+        { text: 'GoPay', value: 'GoPay' }
     ],
     bank: [
-        {
-            text: 'BCA', value: 'BCA'
-        },
-        {
-            text: 'BNI', value: 'BNI'
-        },
-        {
-            text: 'BRI', value: 'BRI'
-        },
-        {
-            text: 'DKI', value: 'DKI'
-        },
-        {
-            text: 'Mandiri', value: 'Mandiri'
-        },
-        {
-            text: 'Mega', value: 'Mega'
-        },
-    ]
+        { text: 'BCA', value: 'BCA' },
+        { text: 'BNI', value: 'BNI' },
+        { text: 'BRI', value: 'BRI' },
+        { text: 'DKI', value: 'DKI' },
+        { text: 'Mandiri', value: 'Mandiri' },
+        { text: 'Mega', value: 'Mega' },
+    ],
+    checkout: {
+        payment: {},
+        address: [],
+        courier: ''
+    }
 }
 
 const getters = {
     paymentMethods: (state) => state.method,
-    bankNames: (state) => state.bank
+    bankNames: (state) => state.bank,
+    payment: (state) => state.checkout.payment,
+    shippingAddress: (state) => state.checkout.address
 }
 
 const actions = {
@@ -53,7 +33,18 @@ const actions = {
 }
 
 const mutations = {
-
+    setPayment: (state, data) => {
+        state.checkout.payment = data
+    },
+    setAddress: (state, data) => {
+        state.checkout.address.push(data)
+    },
+    setCourier: (state, data) => {
+        state.checkout.courier = data
+    },
+    setPaymentMethod: (state, data) => {
+        state.checkout.payment['method'] = data
+    }
 }
 
 export default {
