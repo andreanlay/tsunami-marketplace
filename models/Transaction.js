659cc8e7fe3d38e21dd00c4c555900f492bb38cb
andreanlay <andreanlay1@gmail.com>
2020-11-30 22:36:44 +0700

    Finish transaction schema

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,8 +1,33 @@
 const { Schema, model } = require('mongoose')
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const TransactionSchema = new Schema({
+    _id: {
+        Number
+    },
+    buyer_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'account',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    cart: [
+        {
+            product: {
+                type: Schema.Types.ObjectId,
+                ref: 'product'
+            },
+            price: Number,
+            qty: Number,
+        }
+    ]
+}, {_id: false})
 
-})
+TransactionSchema.plugin(AutoIncrement)
 
 const Transaction = model('transaction', TransactionSchema)
 
