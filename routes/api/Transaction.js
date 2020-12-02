fc981a581ed39be5afff0186497806c750caad2e
andreanlay <andreanlay1@gmail.com>
2020-12-02 22:48:50 +0700

    Add transaction routes

--- routes/api/Transaction.js
+++ routes/api/Transaction.js
@@ -3,4 +3,63 @@ const Transaction = require('../../models/Transaction')
 
 const router = Router()
 
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    
+    try {
+        let transactions = await Transaction.find()
+        
+        if(!transactions) {
+            throw new Error('Error when trying to find transactions..')
+        }
+
+        for(let i=0; i<transactions.length; i++) {
+            await transactions[i].populate('cart.product').execPopulate()
+            await transactions[i].populate('cart.product.seller').execPopulate()
+            await transactions[i].populate('shipping_address').execPopulate()
+
+            if(transactions[i].buyer_id != id) {
+                carts = transactions[i].cart
+                transactions[i].cart = carts.filter(item => item.product.seller == id)
+            }
+        }
+        transactions = transactions.filter(transaction => transaction.cart.length != 0)
+
+        res.status(200).json(transactions)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.post('/', async (req, res) => {
+    const transaction = await new Transaction(req.body)
+
+    try {
+        const TransactionItem = await transaction.save()
+
+        if(!TransactionItem) {
+            throw new Error('Fail to save transaction..')
+        }
+        res.status(200).json(TransactionItem)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const TransactionItem = await Transaction.findOneAndUpdate({_id: id}, req.body, {new: true})
+
+        if(!TransactionItem) {
+            throw new Error('Fail to update transaction')
+        }
+        res.status(200).json(TransactionItem)
+    } catch(err) {
+        console.log(err.message)
+        res.status(500).json({message: err.message})
+    }
+})
+
 module.exports = router
