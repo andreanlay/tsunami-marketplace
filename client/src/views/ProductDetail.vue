0ffeefbdbf3e46e16e6645e95782b22192510cf0
andreanlay <andreanlay1@gmail.com>
2020-12-03 21:18:00 +0700

    Add navbar & description component to product view

--- client/src/views/ProductDetail.vue
+++ client/src/views/ProductDetail.vue
@@ -1,15 +1,40 @@
 <template>
 <div>
-    
+    <b-container fluid :class="{'details-dark': darkMode}">
+        <ProductNavBar/>
+        <ProductDescription/>
+    </b-container>
 </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import { mapGetters } from 'vuex' 
 
+import ProductNavBar from '@/components/ProductDetail/TheNavBar'
+import ProductDescription from '@/components/ProductDetail/Description'
+
+export default {
+    title: 'Product Detail – Tsunami',
+    components: {
+        ProductNavBar,
+        ProductDescription
+    },
+    computed: {
+        ...mapGetters(['darkMode'])
+    },
+    async mounted() {
+        const id = this.$route.params.id
+        await axios.get(`/api/product/details/${id}`)
+        .then(res => {
+            this.$store.commit('setProduct', res.data)
+        })
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.details-dark {
+    color: white;
+}
 </style>
