7ff9b30c3414080e792022b5b2cf52040dbc1e1b
andreanlay <andreanlay1@gmail.com>
2020-12-05 19:49:43 +0700

    First post routes commit

---
+++ routes/api/Post.js
@@ -0,0 +1,22 @@
+const { Router } = require('express')
+const Post = require('../../models/Post')
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+
+})
+
+router.post('/', async (req, res) => {
+
+})
+
+router.get('/reviews', async (req, res) => {
+
+})
+
+router.get('/discussions', async (req, res) => {
+
+})
+
+module.exports = router
