dfa67606402987a466b65d9a6d1dcef8baf0896d
andreanlay <andreanlay1@gmail.com>
2020-12-06 17:12:49 +0700

    Finish review/discussion post route

--- routes/api/Post.js
+++ routes/api/Post.js
@@ -8,15 +8,100 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+    const newPost = new Post(req.body)
 
+    try {
+        const post = await newPost.save()
+
+        if(!post) {
+            throw new Error('Save post failed..')
+        }
+
+        await post.populate('poster').execPopulate()
+        
+        res.status(200).json(post)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
-router.get('/reviews', async (req, res) => {
+router.get('/reviews/:id', async (req, res) => {
+    const id = req.params.id
 
+    try {
+        const posts = await Post.find({
+            product: id,
+            type: 'review'
+        }).sort({posted_date: -1})
+
+        if(!posts) {
+            throw new Error('Fail to find reviews..')
+        }
+
+        for(let i=0; i<posts.length; i++) {
+            await posts[i].populate('poster').execPopulate()
+        }
+        
+        res.status(200).json(posts)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
-router.get('/discussions', async (req, res) => {
+router.get('/discussions/:id', async (req, res) => {
+    const id = req.params.id
 
+    try {
+        const posts = await Post.find({
+            product: id,
+            type: 'discussions'
+        }).sort({posted_date: -1})
+
+        if(!posts) {
+            throw new Error('Fail to find discussions..')
+        }
+        
+        for(let i=0; i<posts.length; i++) {
+            await posts[i].populate('poster').execPopulate()
+            await posts[i].populate('replies').execPopulate()
+
+            for(let j=0; j<posts[i].replies.length; j++) {
+                await posts[i].replies[j].populate('poster').execPopulate()
+            }
+        }
+
+        res.status(200).json(posts)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.post('/discussions/:id/reply', async (req, res) => {
+    const id = req.params.id
+    
+    try {
+        const post = await Post.findOneAndUpdate(
+            {
+                _id: id
+            },
+            {
+                $push: {
+                    replies: req.body._id
+                }
+            },
+            {
+                new: true
+            }
+        )
+        if(!post) {
+            throw new Error('Post reply failed..')
+        }
+        
+        res.status(200).json(post)
+    } catch(err) {
+        console.log(err.message)
+        res.status(500).json({message: err.message})
+    }
 })
 
 module.exports = router
