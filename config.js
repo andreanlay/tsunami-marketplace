9e32a40d933b17e76d7d04b23cd404a07fede957
andreanlay <andreanlay1@gmail.com>
2020-12-08 12:27:42 +0700

    Setting up deployment config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: 'mongodb+srv://admin:TsunamiMarketplace@cluster0.eg5u2.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    PORT: '8081'
+    mongoUri: process.env.MONGO_URI,
+    PORT: process.env.PORT
 }
